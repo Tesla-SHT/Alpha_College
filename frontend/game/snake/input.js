5bbfffdbb8b791e360b02608b6971a133d819d3c
Tesla-SHT <2036230339@qq.com>
2022-11-03 20:07:59 +0800

    complete snake game

--- frontend/game/snake/input.js
+++ frontend/game/snake/input.js
@@ -1,16 +1,16 @@
 let inputDirection = {x:0,y:1};
-let lastInputDirection = {x:0,y:1}
+let lastInputDirection = {x:0,y:1};
 window.addEventListener('keydown',event => {
-    if (event.key === "ArrowUp" && lastInputDirection.y===0) {
+    if (event.key === "ArrowUp" && lastInputDirection.x!==0) {
         inputDirection = {x:0,y:-1};
-    } else if (event.key === "ArrowDown" && lastInputDirection.y===0){
+    } else if (event.key === "ArrowDown" && lastInputDirection.x!==0){
         inputDirection = {x:0,y:1};
-    } else if (event.key === "ArrowLeft" && lastInputDirection.x===0){
+    } else if (event.key === "ArrowLeft" && lastInputDirection.y!==0){
         inputDirection = {x:-1,y:0};
-    } else if (event.key === "ArrowRight" && lastInputDirection.x===0){
+    } else if (event.key === "ArrowRight" && lastInputDirection.y!==0){
         inputDirection = {x:1,y:0};
     }
-})
+});
 const getInputDirection = () => {
     lastInputDirection = inputDirection;
     return inputDirection;
--- frontend/game/snake/snake.js
+++ frontend/game/snake/snake.js
@@ -1,4 +1,4 @@
-const SNAKE_SPEED=5;
+const SNAKE_SPEED=10;
 const snakeBody = [
     {x:11,y:11},
     {x:11,y:10},
@@ -8,11 +8,11 @@ const updateSnake = () => {
 //remove tail segment
     snakeBody.pop();
 //add a new head segment
-    const newHead = {...snakeBody[0]};
-    const snakeDirection = getInputDirection();
-    newHead.x += snakeDirection.x;
-    newHead.y += snakeDirection.y;
-    snakeBody.unshift(newHead);
+const newHead = {...snakeBody[0]};
+const snakeDirection = getInputDirection();
+newHead.x +=snakeDirection.x;
+newHead.y += snakeDirection.y;
+snakeBody.unshift(newHead);
 
 };
 const drawSnake = (gameBoard) => {
